bb0144cc2c35a04c7a49e5c7fba7b0548fb2a0ab
Danny Sanchez <dannysanchez@ymail.com>
2017-12-04 07:23:35 -0500

    Initial commit

---
+++ routes.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+# Create an instance of Flask and save it to the "app" variable.
+app = Flask(__name__)
+
+# Map the URL of / (the root) to the function "index()"
+@app.route("/")
+def index():
+	#When index() is run, render the template file at index.html
+	return render_template("index.html")
+
+# app.run runs it on a local server. This tells it to show debugging messages.
+if __name__ == "__main__":
+	app.run(debug=True)
